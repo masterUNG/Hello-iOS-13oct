d962e0d91876993072b496dbdbc2fecd74d503cf
MasterUNG <phrombutr@gmail.com>
2017-10-14 08:26:50 +0700

    Initial Commit

---
+++ Hello iOS/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by MasterUNG on 13/10/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Explicit
+    var nameString: String = ""
+    
+    
+    @IBOutlet weak var showLabel: UILabel!
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        nameString = myTextField.text!
+        print("Name \(nameString)")
+        
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
